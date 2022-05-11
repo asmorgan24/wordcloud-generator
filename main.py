a635636fda6ef128b40176641f9b0875279bc86d
Andy Morgan <andymorgan@Andys-MacBook-Air.local>
2022-05-10 21:37:52 -0400

    updated gitignore

--- main.py
+++ main.py
@@ -1,60 +1,86 @@
+import sys, os
+import IPython
+from os import walk
+import numpy as np
+import yaml
+
+#application specific src files
+sys.path.insert(1, './src')
 import pdfparser
 import cloud
-import IPython
-import argparse, os, sys
-from os import walk
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    # environment
-    parser.add_argument('--filename', default='test')
+# def parse_args():
+#     parser = argparse.ArgumentParser()
+#     # environment
+#     parser.add_argument('--filename', default='test')
+#
+#     parser.add_argument('--image_height', default=1080, type=int) #2160
+#     parser.add_argument('--image_width', default=1920, type=int) #3840
+#     parser.add_argument('--num_words', default=250, type=int)
+#     parser.add_argument('--background_color', default='black')
+#
+#     parser.add_argument('--save_as_gif', default=False, action='store_true')
+#     parser.add_argument('--interval_duration', default=500, type=int)
+#
+#     args = parser.parse_args()
+#     return args
 
-    parser.add_argument('--image_height', default=1080, type=int) #2160
-    parser.add_argument('--image_width', default=1920, type=int) #3840
-    parser.add_argument('--num_words', default=250, type=int)
-    parser.add_argument('--background_color', default='black')
 
-    parser.add_argument('--save_as_gif', default=False, action='store_true')
-    parser.add_argument('--interval_duration', default=500, type=int)
-
-    args = parser.parse_args()
-    return args
-
-def main():
-    arg = parse_args()
-
+def readPDFs(params):
     #Find all pdf files in 'pdfs' subdirectory
-    path = '.'
+    path = params['proj_path']+'/pdfs'
     pdf_files = []
     for root, directories, files in os.walk(path, topdown=False):
         for name in files:
             if name[-4:]=='.pdf':
                 pdf_files.append(os.path.join(root, name))
-    # Name of the image
-    image_file_name = 'out/'+arg.filename
 
-    total_words = 'a' #initialize
+    #Extract All
+    words = '' #initialize
     for i in range(len(pdf_files)):
         # path to pdf
         path = pdf_files[i]
 
         doc_len = pdfparser.get_doc_length(path)
-        start_page =1
+        start_page =0
         pdf_to_word =pdfparser.get_string_from_pdf(path, start_page, doc_len)
-        total_words+=pdf_to_word
+        words+=pdf_to_word
 
         print ('Processed: ', pdf_files[i])
+    return words
 
-    if arg.save_as_gif == True:
+def readCSVwords(params):
+    w = np.loadtxt(params['proj_path']+'/fakewords.csv', dtype = str, delimiter = ',')
+    words= ""
+    for i in range(len(w)):
+        for _ in range(int(w[i][1])):
+            words+=str(w[i][0])
+            words+='\n'
+    return words
+
+def main():
+    #Load params to for the image creator
+    with open('params.yaml') as f:
+        params = yaml.load(f, Loader=yaml.FullLoader)
+
+    #Set project path directory
+    params['proj_path'] = os.getcwd() if params['proj_path'] == 'None' else params['proj_path']
+
+    # Name of the savable image, without the filetype
+    image_save_location = params['proj_path']+'/out/'+params['saved_filename']
+
+    if params['use_pdfs'] == True: #use the pdf for accumulating words
+        text = readPDFs(params)
+    else: #use the csv file for accumulating words
+        text = readCSVwords(params)
+
+    if params['save_as_gif'] == True:
         print ('Saving as .GIF, please be patient...')
 
     # If you want to exclude certain words from the cloud,
     # you can add them as a new line to the file stopwords.txt
-    cloud.makecloud(total_words, arg.image_width,
-                    arg.image_height, arg.num_words, image_file_name,
-                    arg.background_color, arg.save_as_gif,
-                    arg.interval_duration)
-    print ('Alles fertig!')
+    cloud.makecloud(text, params, image_save_location)
+    print ('Alles fertig! All done!')
 
 if __name__ == '__main__':
     main()
